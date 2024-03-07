ad86c0fc6a4ecf5d994c10d2d1593077951e4a88
jhmnn <jhmnn@yandex.ru>
2024-03-07 10:37:05 +0700

    Implement Input class

---
+++ src/engine/engine/input.cpp
@@ -0,0 +1,44 @@
+#include <engine/input.hpp>
+
+#include <ncurses.h>
+
+Input::Key Input::keycode_to_key(int kc) {
+  switch (kc) {
+  case 'w':
+    return Key::W;
+  case 'a':
+    return Key::A;
+  case 's':
+    return Key::S;
+  case 'd':
+    return Key::D;
+  case 'q':
+    return Key::Q;
+  case 'r':
+    return Key::R;
+  default:
+    return Key::Unknown;
+  }
+}
+
+bool Input::is_pressed(Key key) {
+  return (keys_states_ >> static_cast<int>(key)) == 1;
+}
+
+std::vector<Input::Key> &Input::pressed() { return pressed_; }
+
+Input::Key Input::pressed_last() { return pressed_last_; }
+
+void Input::input() {
+  current_key_ = keycode_to_key(getch());
+  keys_states_ |= (1 << static_cast<int>(current_key_));
+  pressed_last_ = current_key_;
+  if (current_key_ != Key::Unknown) {
+    pressed_.push_back(current_key_);
+  }
+}
+
+void Input::update() {
+  keys_states_ = 0;
+  pressed_last_ = Key::Unknown;
+}
---
+++ src/engine/engine/input.hpp
@@ -0,0 +1,31 @@
+#pragma once
+
+#include <vector>
+
+#include <cstdint>
+
+class Engine;
+
+class Input {
+  friend class Engine;
+
+public:
+  enum class Key { W, A, S, D, Q, R, Unknown };
+
+public:
+  static bool is_pressed(Key key);
+  static std::vector<Key> &pressed();
+  static Key pressed_last();
+
+private:
+  static uint32_t keys_states_;
+  static Key current_key_;
+  static std::vector<Key> pressed_;
+  static Key pressed_last_;
+
+private:
+  static Key keycode_to_key(int kc);
+
+  static void input();
+  static void update();
+};
